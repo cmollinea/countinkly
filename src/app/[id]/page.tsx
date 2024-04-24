27f22f558d371baaf9bf1117c2f1652c2dbecab0
cmollinea <mollinea99@gmail.com>
2024-04-24 00:57:16 -0400

    display errors

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -55,27 +55,54 @@ async function VisitCounter({ params, searchParams }: Props) {
 	const realIp = headers().get("x-real-ip");
 	// biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
 	let country;
+	// biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
+	let error;
 
-	if (forwardedFor || realIp) {
+	// if (forwardedFor || realIp) {
+	// 	const res = await fetch(`http://ip-api.com/json/${forwardedFor || realIp}`);
+	// 	console.log(res);
+
+	// 	if (!res.ok) {
+	// 		throw new Error("Unknown Error");
+	// 	}
+	// 	country = await res.json();
+
+	// 	if (country.data.origin?.status === "fail") {
+	// 		throw new Error("Unknown Error");
+	// 	}
+	// }
+
+	// createRecord(link.id, link.userId, searchParams.source || "Unknown");
+	// incrementView(link.id);
+	// redirect(link.url);
+	const res = await fetch(`http://ip-api.com/json/${forwardedFor || realIp}`);
+	const a = await res.json();
+
+	try {
 		const res = await fetch(`http://ip-api.com/json/${forwardedFor || realIp}`);
 		if (!res.ok) {
 			throw new Error("Unknown Error");
 		}
 		country = await res.json();
 
-		if (country.data.origin.status === "fail") {
-			throw new Error("Unknown Error");
+		if (country.status === "fail") {
+			throw new Error(country.message);
+		}
+	} catch (err) {
+		if (err instanceof Error) {
+			error = err.message;
+		} else {
+			error = "Unknown Error";
 		}
 	}
-
-	// createRecord(link.id, link.userId, searchParams.source || "Unknown");
-	// incrementView(link.id);
-	// redirect(link.url);
 	return (
 		<div className="flex flex-col space-y-10 items-center place-content-center">
 			<p>{forwardedFor}</p>
 			<p>{realIp}</p>
-			<code className="max-w-lg w-full">{country}</code>
+			{country && (
+				<code className="max-w-lg w-full">{JSON.stringify(country)}</code>
+			)}
+			{error && <p>{error}</p>}
 		</div>
 	);
 }
