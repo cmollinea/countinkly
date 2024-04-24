5f309daf68ca51893bed75406ed11137926c6b12
cmollinea <mollinea99@gmail.com>
2024-04-24 01:17:27 -0400

    #1 try to create record

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,7 +4,6 @@ import { incrementView } from "@/lib/increment-view";
 import prisma from "@/lib/prisma";
 import { Metadata } from "next";
 import { notFound, redirect } from "next/navigation";
-import { headers } from "next/headers";
 
 type Props = {
 	params: {
@@ -46,64 +45,14 @@ export async function generateMetadata({
 }
 
 async function VisitCounter({ params, searchParams }: Props) {
-	// const link = await getOriginalUrl(params.id);
-	// if (typeof link === "string") {
-	// 	return notFound();
-	// }
-
-	const forwardedFor = headers().get("x-forwarded-for");
-	const realIp = headers().get("x-real-ip");
-	// biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
-	let country;
-	// biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
-	let error;
-
-	// if (forwardedFor || realIp) {
-	// 	const res = await fetch(`http://ip-api.com/json/${forwardedFor || realIp}`);
-	// 	console.log(res);
-
-	// 	if (!res.ok) {
-	// 		throw new Error("Unknown Error");
-	// 	}
-	// 	country = await res.json();
-
-	// 	if (country.data.origin?.status === "fail") {
-	// 		throw new Error("Unknown Error");
-	// 	}
-	// }
-
-	// createRecord(link.id, link.userId, searchParams.source || "Unknown");
-	// incrementView(link.id);
-	// redirect(link.url);
-	const res = await fetch(`http://ip-api.com/json/${forwardedFor || realIp}`);
-	const a = await res.json();
-
-	try {
-		const res = await fetch(`http://ip-api.com/json/${forwardedFor || realIp}`);
-		if (!res.ok) {
-			throw new Error("Unknown Error");
-		}
-		country = await res.json();
-
-		if (country.status === "fail") {
-			throw new Error(country.message);
-		}
-	} catch (err) {
-		if (err instanceof Error) {
-			error = err.message;
-		} else {
-			error = "Unknown Error";
-		}
+	const link = await getOriginalUrl(params.id);
+	if (typeof link === "string") {
+		return notFound();
 	}
-	return (
-		<div className="flex flex-col space-y-10 items-center place-content-center">
-			<p>{forwardedFor}</p>
-			<p>{realIp}</p>
-			{country && (
-				<code className="max-w-lg w-full">{JSON.stringify(country)}</code>
-			)}
-			{error && <p>{error}</p>}
-		</div>
-	);
+
+	createRecord(link.id, link.userId, searchParams.source || "Unknown");
+	incrementView(link.id);
+	redirect(link.url);
+	return null;
 }
 export default VisitCounter;
--- src/lib/create-record.ts
+++ src/lib/create-record.ts
@@ -1,40 +1,54 @@
 import { headers } from "next/headers";
 import prisma from "@/lib/prisma";
 
+type IpResponse = {
+	status?: string;
+	country?: string;
+	countryCode?: string;
+	region?: string;
+	regionName?: string;
+	city?: string;
+	zip?: string;
+	lat?: number;
+	lon?: number;
+	timezone?: string;
+	isp?: string;
+	org?: string;
+	as?: string;
+	query?: string;
+	message?: string;
+	error?: string;
+};
 
 export async function createRecord(
 	linkId: string,
 	userId: string,
 	source: string,
 ) {
-	let country: any;
+	let ipResponse: IpResponse;
 	const forwardedFor = headers().get("x-forwarded-for");
 	const realIp = headers().get("x-real-ip");
 
 	try {
-		if (forwardedFor || realIp) {
-			const res = await fetch(
-				`http://ip-api.com/json/${forwardedFor ? forwardedFor.split(",")[0].trim() : realIp ? realIp.trim() : ''}`,
-			)
-			if(!res.ok) {
-				throw new Error('Unknown Error')
-			}
-			country = await res.json()
+		const res = await fetch(`http://ip-api.com/json/${forwardedFor || realIp}`);
+		if (!res.ok) {
+			throw new Error("Unknown Error");
+		}
+		ipResponse = await res.json();
 
-			if (country.data.origin.status === 'fail') {
-				throw new Error('Unknown Error')
-			}
-				
-		}  else {
-			country = "Unknown";
+		if (ipResponse.status === "fail") {
+			throw new Error(ipResponse.message);
 		}
 	} catch (err) {
-		country = 'Unknown'
+		if (err instanceof Error) {
+			console.log(err);
+		}
+		ipResponse = { error: "Unknown" };
 	}
 
 	const record = await prisma?.clicks.create({
 		data: {
-			origin: (country as string) || "",
+			origin: (ipResponse.country as string) || "",
 			timestamp: new Date(),
 			source,
 			linkId: linkId,
